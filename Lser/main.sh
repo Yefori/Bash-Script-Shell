6d0eae6f3f50b17b2b07d5838c87b18f87927727
Yefori <yearning.freedom2002@gmail.com>
2018-04-20 20:58:30 +0800

    Update main.sh

--- Lser/main.sh
+++ Lser/main.sh
@@ -1,6 +1,11 @@
+wget https://raw.githubusercontent.com/Yefori/Shell-Scripts/master/Lser/scanner.py -O scanner.py
+python scanner.py 43.225.100.0/24 -t 50 -T 5 -p 22 > list
+
+
+
 function  for_in_file(){
-For  i  in  `cat $FILENAME`
+For  i  in  `cat list`
 do
-echo $i
+
 done
 }
