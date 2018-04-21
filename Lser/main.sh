bfd84dc83214bb356046963324f668064d8b4183
Yefori <yearning.freedom2002@gmail.com>
2018-04-21 09:41:10 +0800

    Update main.sh

--- Lser/main.sh
+++ Lser/main.sh
@@ -1,10 +1,22 @@
+#!/bin/bash
+
 wget https://raw.githubusercontent.com/Yefori/Shell-Scripts/master/Lser/scanner.py -O scanner.py
 python scanner.py 43.225.100.0/24 -t 50 -T 5 -p 22 > list
 
-
-
+echo -e '' > result
 function  for_in_file(){
 For  i  in  `cat list`
+echo -e '\n\n' >> result
+/usr/local/bin/expect <<-EOF
+set timeout 2
+spawn ssh root@$i
+expect {
+"*yes/no" { send "yes\r"; exp_continue }
+"*password:" { send "default4321\r" }
+}
+send "free\r">>result
+expect eof
+EOF
 do
 
 done
